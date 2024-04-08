43e849f6daba5a2d0d6218b632b0510c58db0c5f
Emilse84 <115670726+Emilse84@users.noreply.github.com>
2024-04-08 10:03:37 -0300

    Add files via upload

---
+++ Viejo/js.js
@@ -0,0 +1,15 @@
+
+/*console.log("Hola Mundo!!");
+console.log("Clase 1 JavaScript");
+console.log(22538);
+console.log(2+2);
+
+console.log("Bienvenida Comisión: "+ 22538);
+*/
+
+let alumno = "Emilse";
+
+/*let define la variable, en este caso el nombre de ésta es "alumno";
+el = le asigna un valor, en este caso el nombre es "Emilse"*/
+
+console.log(alumno);
